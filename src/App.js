a1a0aea9adfd91ed0199e81cd7f0c9b1cdae6aa2
Kakie_Wong <kakiewong@gmail.com>
2021-03-08 20:59:39 -0800

    Add menu components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React, { useState } from 'react'
 import './App.css';
-import Header from './components/header/Header'
-import Budget from './components/budget/Budget'
-
+import Header from './components/Header/Header'
+import Budget from './components/Budget/Budget'
+import Slider from './components/Slider/Slider'
 
 function App() {
+  const [budget, setBudget] = useState(0)
+  const [transactions, setTransaction] = useState([])
+
   return (
     <div className="app">
       <Header />
+      <Slider />
       <div className='showcase'>
-        <Budget />
+        <Budget budget={budget} />
         <div className='view-toggle'>
           <button>Transactions</button>
           <button>Breakdown</button>
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Slider.css'
+import AddTransaction from '../AddTransaction/AddTransaction'
+import AddBalance from '../AddBalance/AddBalance'
+
+function Slider() {
+    return (
+        <div className='slider out'>
+            <h1>x</h1>
+            <AddTransaction />
+        </div>
+    )
+}
+
+export default Slider
